f19f6aa8e97ceb6e300dfbd0d6b957486147f9dd
sabin149 <dangalsabin149@gmail.com>
2023-03-02 22:17:52 +0545

    mui text field

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
-import TestComp from "TestComp";
+import { TextField } from "@mui/material";
 
 const inputStyles = {
   width: "300px",
@@ -15,19 +15,31 @@ const inputStyles = {
   marginBottom: "1.6rem",
 };
 
+const buttonStyles = {
+  width: "300px",
+  height: "36px",
+  borderRadius: "3px",
+  border: "2px solid #000",
+  outline: "none",
+  padding: "0 10px",
+  fontSize: "16px",
+  marginBottom: "1.6rem",
+  cursor: "pointer",
+  backgroundColor: "#043367",
+  color: "#fff",
+};
+
 export type formType = {
   name: string;
   email: string;
+  age: number;
 };
 
 const Form = () => {
-  const [user, setUser] = useState<formType>({
-    name: "",
-    email: "",
-  });
   const schema = yup.object().shape({
     name: yup.string().required("Name is required"),
     email: yup.string().email().required("Valid email is required"),
+    age: yup.number().required("Age is required"),
   });
 
   const {
@@ -36,35 +48,15 @@ const Form = () => {
     formState: { errors },
     reset,
     setValue,
-    getValues, // added this to set default values after the user data is fetched
+    getValues,
+    control,
   } = useForm<formType>({
     mode: "onSubmit",
     resolver: yupResolver(schema),
   });
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/users/1"
-      );
-      const data = await response.json();
-      setValue("name", data.name);
-      setValue("email", data.email);
-    };
-    // fetchUser();
-  }, []);
-
-  //   useEffect(() => {
-  //     setValue("name", user.name);
-  //     setValue("email", user.email);
-  //   }, [user, setValue]);
-
   const handleOnSubmit = async (data: formType) => {
-    // console.log({ data });
-    // console.log(getValues());
-    // resetField("name");
-    //    clear form field
-
+    console.log(data);
     reset();
   };
 
@@ -81,55 +73,72 @@ const Form = () => {
       }}
     >
       <form onSubmit={handleSubmit(handleOnSubmit)}>
-        <input
-          type="text"
-          placeholder="Name"
-          {...register("name")}
-          style={inputStyles}
-          autoComplete="off"
-        />
-        <p
-          style={{
-            color: "red",
-          }}
-        >
-          {errors.name?.message}
-        </p>
-        <input
-          type="text"
-          placeholder="Email"
-          {...register("email")}
-          style={inputStyles}
-          autoComplete="off"
-        />
-        <p
-          style={{
-            color: "red",
-          }}
-        >
-          {errors.email?.message}
-        </p>
-
-        <button
-          style={{
-            width: "300px",
-            height: "36px",
-            borderRadius: "3px",
-            border: "2px solid #000",
-            outline: "none",
-            padding: "0 10px",
-            fontSize: "16px",
-            marginBottom: "1.6rem",
-            cursor: "pointer",
-            backgroundColor: "#043367",
-            color: "#fff",
-          }}
-          type="submit"
-        >
+        <div className="name__div">
+          <input
+            type="text"
+            placeholder="Name"
+            {...register("name")}
+            style={inputStyles}
+            autoComplete="off"
+          />
+          <p
+            style={{
+              color: "red",
+            }}
+          >
+            {errors.name?.message}
+          </p>
+        </div>
+        <div className="email__div">
+          <input
+            type="text"
+            placeholder="Email"
+            {...register("email")}
+            style={inputStyles}
+            autoComplete="off"
+          />
+          <p
+            style={{
+              color: "red",
+            }}
+          >
+            {errors.email?.message}
+          </p>
+        </div>
+        <div className="age__div">
+          <Controller
+            control={control}
+            name="age"
+            render={({ field: { ref, onChange } }) => (
+              <TextField
+                label="Age"
+                variant="outlined"
+                size="small"
+                type="number"
+                ref={ref}
+                // onChange={onChange}
+                sx={{
+                  width: "300px",
+                  height: "36px",
+                  fontSize: "16px",
+                  marginBottom: "1.6rem",
+                }}
+                autoComplete="off"
+              />
+            )}
+          />
+          <p
+            style={{
+              color: "red",
+            }}
+          >
+            {errors.age?.message}
+          </p>
+        </div>
+        <button style={buttonStyles} type="submit">
           Submit
         </button>
       </form>
-      <TestComp formData={getValues()} />
     </main>
   );
 };
